c0d7ed9055f4baad6c158f2c566dea08003fe899
ThibaultYVD <thibaultyvard01@gmail.com>
2023-01-21 23:37:44 +0100

    ajout commande /dés

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const server = require("./server")
 const Discord = require("discord.js");
 const SoraBot = new Discord.Client({ intents: 3276799 });
 const loadCommands = require("./config/loadCommands")
