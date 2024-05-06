7664c854da28f4f25a9dabbf4c17600ca5b16444
Thibault Yvard <thibaultyvd@vmi998189.contaboserver.net>
2024-05-06 10:21:59 +0200

    cancelled connection pool

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql");
 
 module.exports = async() =>{
-  let db = mysql.createPool({
+  let db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
