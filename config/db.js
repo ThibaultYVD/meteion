78cde738960c3b10e43da614888243b25c069312
ThibaultYVD <thibaultyvard01@gmail.com>
2024-10-09 10:28:16 +0200

    moving code from client/ to root folder

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-const mysql = require("mysql");
-
-module.exports = async() =>{
-  let db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    charset : 'utf8mb4',
-  });
-
-  return db;
-}
+module.exports = {
+	HOST: process.env.DB_HOST,
+	USER: process.env.DB_USER,
+	PASSWORD: process.env.DB_PASSWORD,
+	DB: process.env.DB_NAME,
+	dialect: 'mysql',
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+};
--- events/ready.js
+++ events/ready.js
@@ -1,37 +1,9 @@
-const loadSlashCommands = require("../config/loadSlashCommands");
-const loadDB = require("../config/db")
-const createEventReminderJob = require("../jobs/eventReminder")
-const createCloseEventJob = require("../jobs/closeEvent")
-const { createInfoLog, createWarnLog, createErrorLog } = require("../modules/logs")
+const { Events } = require('discord.js');
 
-module.exports = async (client) => {
-
-    try {
-        client.db = await loadDB()
-
-        client.db.connect(function (error) {
-            if (error) {
-                console.log(error +
-                    '\n/!\\ ERROR : Connexion à la base de données impossible. /!\\\nMerci de vérifier les variables de connexion dans le fichier .env.' +
-                    `\nLe lancement de ${client.user.tag} a échoué.`)
-
-                process.exit(0);
-            }
-            console.log('\nConnecté à la base de données MySQL.\n');
-
-
-        }),
-            await loadSlashCommands(client)
-
-        // Lancement des jobs
-        const eventReminderJob = createEventReminderJob(client);
-        const closeEventJob = createCloseEventJob(client);
-        eventReminderJob.start()
-        closeEventJob.start()
-
-        console.log(`\n${client.user.tag} est en ligne.`);
-    } catch (error) {
-        createErrorLog(client, `Le lancement du client a échoué.`, "events/ready.js", "null")
-    }
-
-}
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`\nReady! Logged in as ${client.user.tag}`);
+	},
+};
