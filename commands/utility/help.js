c1d0d8657a3c2dc027dfed84ba6c6498b09f315f
sorasilver-dev <sorasilver4788@gmail.com>
2024-10-16 22:40:58 +0200

    ajout help et début job

---
+++ commands/utility/help.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Liste les commandes disponibles.')
+		.setDescriptionLocalizations({
+			'en-US': 'List the available commands.',
+		}),
+	async execute(interaction) {
+		const embed = new EmbedBuilder()
+			.setColor(interaction.client.color)
+			.setTitle('Centre d\'aide - Commandes')
+			.addFields(
+				{ name: '`/help`', value: '> Aide & Ressources.' },
+				{ name: '`/event`', value: '> Création d\'un événement.' },
+				{ name: '\u200B', value: '\u200B' })
+			.setFooter({
+				text: 'Made with ❤️ by @sorasilver_',
+				iconURL: interaction.client.user.displayAvatarURL({ dynamic: true }),
+			});
+
+		await interaction.reply({ embeds: [embed], ephemeral: true });
+	},
+};
