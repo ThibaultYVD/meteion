0716bc9cb5837d6866912386c3672c5bbc3cc7a4
ThibaultYVD <thibaultyvard01@gmail.com>
2024-11-30 11:24:47 +0100

    preparation for production

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ module.exports = {
 	PASSWORD: process.env.DB_PASSWORD,
 	DB: process.env.DB_NAME,
 	dialect: 'mysql',
+	logging: false,
 	pool: {
 		max: 5,
 		min: 0,
 		acquire: 30000,
 		idle: 10000,
 	},
-	logging: false,
 };
